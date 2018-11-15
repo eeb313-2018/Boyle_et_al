b8782eb4a004080d277efa56ed5bc93c88d41e81
colebrookson <cole.brookson@mail.utoronto.ca>
2018-11-15 15:44:33 -0500

    Update EEB313_project_code.R

--- EEB313_project_code.R
+++ EEB313_project_code.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-# setwd("your local working directory")
+setwd("C:/Users/coleb/Documents/GitHub/Boyle_et_al-master/Boyle_et_al")
 
 ## Always use this code to start so that we have the same column names and cleaned data
 predprey <- read_csv("predprey.csv",col_names = TRUE)
@@ -15,22 +15,43 @@ names(predprey) <- sub("_._", ".", names(predprey)) #fix / in some column names
 View(predprey)
 
 # Time to check for values which don't make sense
-unique(predprey$Predator_length_unit) #needs fixing to all mm, \xb5m is unicode for µm
+
+######predatorlength 
+unique(predprey$Predator_length_unit)#needs fixing to all mm, \xb5m is unicode for µm
 
 
+predprey <- predprey %>% mutate(multiplierpredlengthunit = case_when(
+  Predator_length_unit == 'mm'~ 1,
+  Predator_length_unit == 'cm' ~ 0.1,
+  Predator_length_unit == 'um' ~ 0.01))
+predprey <- predprey %>% mutate(Predator_length = Predator_length/multiplierpredlengthunit) 
+
+######prey length 
 unique(predprey$Prey_length_unit) # needs fixing to all mm, \xb5m is unicode for µm
+predprey <- predprey %>% mutate(multiplierpreylengthunit = case_when(
+  Prey_length_unit == 'mm'~ 1,
+  Prey_length_unit == 'cm' ~ 0.1,
+  Prey_length_unit == 'um' ~ 0.01))
+predprey <- predprey %>% mutate(Prey_length = Predator_length/multiplierpreylengthunit) 
 
+unique(predprey$Predator_mass_unit) 
 
+######prey mass
 unique(predprey$Prey_mass_unit) # must convert to all mg
+predprey <- predprey %>% mutate(multiplierpreymassunit = case_when(
+  Prey_mass_unit == 'mg'~ 0.001,
+  Prey_mass_unit == 'g'~ 1))
+predprey <- predprey %>% mutate(Prey_mass = Prey_mass/multiplierpreymassunit) 
 
 
 unique(predprey$Prey_conversion_to_mass_reference) # sub 'n/a' with blank
-gsub("n/a", "",predprey$Prey_conversion_to_mass_reference)
+predprey$Prey_conversion_to_mass_reference <- gsub("n/a", " ",predprey$Prey_conversion_to_mass_reference)
 unique(predprey$Prey_conversion_to_mass_reference)
 
 unique(predprey$Specific_habitat) #Correct for lower and uppercase shelf
-gsub("shelf","Shelf",predprey$Specific_habitat)
+predprey$Specific_habitat <- gsub("shelf","Shelf",predprey$Specific_habitat)
 unique(predprey$Specific_habitat)
 
 
 #Convert latitude and longitude to decimal values
+
